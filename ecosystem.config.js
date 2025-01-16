f31bbc5d61d83b2a0a91f81b717f267aa1dabe29
Deeparth Gupta <dprthgpta@gmail.com>
2025-01-16 17:41:22 +0530

    Added deployment via github actions and added a pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [{
+    name: "dharaniDairy",          // Application name
+    script: "./server.js",         // Server
+    watch: true,                   // Watch for file changes
+    env: {
+      NODE_ENV: "production",
+      PORT: 5000                   
+    },
+    max_memory_restart: "500M",    // Restart if memory exceeds 500M
+  }]
+};
